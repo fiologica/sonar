ce9bb98ec31723e26523337c6338ff381dbb3513
fiologica <39060769+fiologica@users.noreply.github.com>
2023-01-19 21:00:42 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,11 @@
-let sonar2 = sonar.ping(
-DigitalPin.P8,
-DigitalPin.P10,
-PingUnit.Centimeters
-) / 10
-basic.showNumber(sonar2)
 servos.P1.setRange(0, 180)
 servos.P2.setRange(0, 180)
 basic.forever(function () {
-    sonar2 += sonar.ping(
-    DigitalPin.P8,
-    DigitalPin.P10,
-    PingUnit.Centimeters
-    ) / 10
-    basic.showNumber(sonar2)
-    if (sonar2 >= 5) {
-        servos.P1.run(50)
-        servos.P2.run(50)
-    } else {
-        servos.P1.stop()
-        servos.P2.stop()
+    makerbit.connectUltrasonicDistanceSensor(DigitalPin.P8, DigitalPin.P10)
+    basic.showNumber(makerbit.getUltrasonicDistance(DistanceUnit.CM))
+    if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 200) {
+        kitronik_servo_lite.forward()
+    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) < 100) {
+        kitronik_servo_lite.stop()
     }
 })
