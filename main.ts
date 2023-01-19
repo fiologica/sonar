08aa26246f7e651a536ccec93331c3d282e09171
fiologica <39060769+fiologica@users.noreply.github.com>
2023-01-19 21:30:33 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,13 @@ input.onButtonPressed(Button.A, function () {
 })
 makerbit.onUltrasonicObjectDetected(100, DistanceUnit.CM, function () {
     kitronik_servo_lite.stop()
+    kitronik_servo_lite.driveBackwards(10)
+    kitronik_servo_lite.stop()
+    basic.pause(100)
 })
 servos.P1.setRange(0, 180)
 servos.P2.setRange(0, 180)
+kitronik_servo_lite.setDistancePerSecond(10)
 basic.forever(function () {
     makerbit.connectUltrasonicDistanceSensor(DigitalPin.P8, DigitalPin.P10)
     basic.showNumber(makerbit.getUltrasonicDistance(DistanceUnit.CM))
