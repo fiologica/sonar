60cb7ef4eee2049dd12b142d8fc75a54e02adbe4
fiologica <39060769+fiologica@users.noreply.github.com>
2023-01-19 21:47:44 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,13 @@ input.onButtonPressed(Button.A, function () {
     control.reset()
 })
 makerbit.onUltrasonicObjectDetected(100, DistanceUnit.CM, function () {
-    kitronik_servo_lite.stop()
-    kitronik_servo_lite.driveBackwards(10)
-    kitronik_servo_lite.stop()
-    basic.pause(100)
+    if (makerbit.isUltrasonicDistanceLessThan(60, DistanceUnit.CM)) {
+        kitronik_servo_lite.stop()
+        basic.pause(100)
+    } else {
+        kitronik_servo_lite.driveBackwards(10)
+        basic.pause(100)
+    }
 })
 servos.P1.setRange(0, 180)
 servos.P2.setRange(0, 180)
@@ -15,7 +18,7 @@ basic.forever(function () {
     basic.showNumber(makerbit.getUltrasonicDistance(DistanceUnit.CM))
     if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 200) {
         kitronik_servo_lite.forward()
-    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) < 100) {
+    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 100) {
         kitronik_servo_lite.stop()
     }
 })
