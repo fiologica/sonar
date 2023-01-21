d751b0d35f770d0b0ebc7f4562f003a3473d7a48
fiologica <39060769+fiologica@users.noreply.github.com>
2023-01-21 20:28:06 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let sonar2 = 0
 let distance = 0
-basic.forever(function () {
+loops.everyInterval(1, function () {
     sonar2 = sonar.ping(
     DigitalPin.P8,
     DigitalPin.P10,
@@ -12,6 +12,8 @@ basic.forever(function () {
     } else if (distance > 20) {
         kitronik_servo_lite.forward()
     }
+})
+basic.forever(function () {
     basic.showNumber(distance)
     serial.writeValue("Distance", distance)
 })
