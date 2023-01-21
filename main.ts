b2b167b0849aa796364c6da93c3b8705ca3efc26
fiologica <39060769+fiologica@users.noreply.github.com>
2023-01-21 19:56:02 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,17 @@
-input.onButtonPressed(Button.A, function () {
-    control.reset()
-})
-makerbit.onUltrasonicObjectDetected(100, DistanceUnit.CM, function () {
-    if (makerbit.isUltrasonicDistanceLessThan(60, DistanceUnit.CM)) {
-        kitronik_servo_lite.stop()
-        basic.pause(100)
-    } else {
-        kitronik_servo_lite.driveBackwards(10)
-        basic.pause(100)
-    }
-})
-servos.P1.setRange(0, 180)
-servos.P2.setRange(0, 180)
-kitronik_servo_lite.setDistancePerSecond(10)
+let sonar2 = 0
+let distance = 0
 basic.forever(function () {
-    makerbit.connectUltrasonicDistanceSensor(DigitalPin.P8, DigitalPin.P10)
-    basic.showNumber(makerbit.getUltrasonicDistance(DistanceUnit.CM))
-    if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 200) {
-        kitronik_servo_lite.forward()
-    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 100) {
+    sonar2 = sonar.ping(
+    DigitalPin.P8,
+    DigitalPin.P10,
+    PingUnit.MicroSeconds
+    )
+    distance = sonar2 * 0.034 / 10
+    basic.showNumber(distance)
+    serial.writeValue("Distance", distance)
+    if (distance <= 20) {
         kitronik_servo_lite.stop()
+    } else if (distance > 20) {
+        kitronik_servo_lite.forward()
     }
 })
